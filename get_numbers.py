78a68056c2ce311b638cc692f8c9232d78f26063
rakhat617 <rakhat.as617@gmail.com>
2025-10-21 17:26:33 +0500

    21.10

--- get_numbers.py
+++ get_numbers.py
@@ -7,6 +7,8 @@ def get_numbers_func(url):
     numbers = []
 
     for message in messages:
+        if "@g.us" in message["chatId"]:
+            continue
         sender_id = message.get("senderId")
         sender_number = sender_id.split("@")[0]
         if sender_number not in numbers:
